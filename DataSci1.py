d8965e02b7f20355ffc91da3ff9d72ca3782a0db
jjeble <jayj2019@gmail.com>
2017-07-27 13:52:59 -0700

    Update DataSci1.py

--- DataSci1.py
+++ DataSci1.py
@@ -1,7 +1,15 @@
+
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
 df = pd.read_csv("/Users/jayjeble/Documents/SZ/train.csv")
-df['ApplicantIncome'].hist(bins=50)
-plt.show()
+temp1 = df['Credit_History'].value_counts(ascending=True)
+temp2 = df.pivot_table(values="Loan_Status",index=['Credit_History'],aggfunc = lambda x:x.map({'Y':1,'N':0}).mean())
+
+print('Frequency table for credit history')
+print(temp1)
+
+print('\nProbility of getting loan for each Credit History class:')
+print(temp2)
+#plt.show()
