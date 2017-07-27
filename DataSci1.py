8eac39c6c1e8bf5dd001b7f756f4dc61daff111c
jjeble <jayj2019@gmail.com>
2017-07-27 14:04:47 -0700

    Update DataSci1.py

--- DataSci1.py
+++ DataSci1.py
@@ -12,4 +12,22 @@ print(temp1)
 
 print('\nProbility of getting loan for each Credit History class:')
 print(temp2)
-#plt.show()
+
+
+fig = plt.figure(figsize=(8,4))
+ax1 = fig.add_subplot(121)
+ax1.set_xlabel('Credit_History')
+ax1.set_ylabel('Count of Applicants')
+ax1.set_title("Applicants by Credit History")
+temp1.plot(kind="bar")
+
+
+ax2 = fig.add_subplot(122)
+ax2.set_xlabel('Credit_History')
+ax2.set_ylabel('Probability of getting loan')
+ax2.set_title("Probability of getting loan by credit history")
+temp2.plot(kind = 'bar')
+
+
+plt.show()
+
