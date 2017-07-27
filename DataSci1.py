a251610f91cd94a4c65f683103054b1c453ead97
jjeble <jayj2019@gmail.com>
2017-07-27 12:52:09 -0700

    Update DataSci1.py

--- DataSci1.py
+++ DataSci1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+from matplotlib import pyplot as plt
 
 df = pd.read_csv("/Users/jayjeble/Documents/SZ/train.csv")
-
+df['ApplicantIncome'].hist(bins=50)
+plt.show()
