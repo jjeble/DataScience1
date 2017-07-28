6c71d7011d706233ad0c701b9c9e6619cea6f4a3
jjeble <jayj2019@gmail.com>
2017-07-28 11:57:50 -0700

    Update DataSci1.py

--- DataSci1.py
+++ DataSci1.py
@@ -7,13 +7,13 @@ df = pd.read_csv("/Users/jayjeble/Documents/SZ/train.csv")
 
 print(df.apply(lambda x: sum(x.isnull()),axis=0))
 
-df['LoanAmount'].fillna(df['LoanAmount'].mean(), inplace = True)
-
-print(df.apply(lambda x: sum(x.isnull()),axis=0))
-
-df.boxplot(column="LoanAmount",by=["Education","Self_Employed"])
-
 df['Self_Employed'].fillna('No',inplace=True)
 
-
+table = df.pivot_table(values='LoanAmount', index='Self_Employed' ,columns='Education', aggfunc=np.median)
+# Define function to return value of this pivot_table
+def fage(x):
+ return table.loc[x['Self_Employed'],x['Education']]
+# Replace missing values
+df['LoanAmount'].fillna(df[df['LoanAmount'].isnull()].apply(fage, axis=1), inplace=True)
+print(df.apply(lambda x: sum(x.isnull()),axis=0))
 plt.show()
