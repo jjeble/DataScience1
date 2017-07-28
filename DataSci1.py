314748d6d84264f31175e7d9c64bb9f4ddfa0c60
jjeble <jayj2019@gmail.com>
2017-07-28 12:21:57 -0700

    Update DataSci1.py

--- DataSci1.py
+++ DataSci1.py
@@ -7,13 +7,20 @@ df = pd.read_csv("/Users/jayjeble/Documents/SZ/train.csv")
 
 print(df.apply(lambda x: sum(x.isnull()),axis=0))
 
-df['Self_Employed'].fillna('No',inplace=True)
+print(df['Gender'].value_counts())
+print(df['Dependents'].value_counts())
 
-table = df.pivot_table(values='LoanAmount', index='Self_Employed' ,columns='Education', aggfunc=np.median)
-# Define function to return value of this pivot_table
+
+df['Self_Employed'].fillna('No',inplace=True)
+df['Gender'].fillna('Male',inplace=True)
+df['Dependents'].fillna(0,inplace=True)
+
+table = df.pivot_table(values='LoanAmount', index='Self_Employed' ,columns=['Gender'], aggfunc=np.median)
+print(table)
 def fage(x):
- return table.loc[x['Self_Employed'],x['Education']]
-# Replace missing values
+ return table.loc[x['Self_Employed'],x['Gender']]
+##
+
 df['LoanAmount'].fillna(df[df['LoanAmount'].isnull()].apply(fage, axis=1), inplace=True)
 print(df.apply(lambda x: sum(x.isnull()),axis=0))
 plt.show()
