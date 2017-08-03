403a5221c5ca2b85289c37257e2b8e473b2af28d
jjeble <jayj2019@gmail.com>
2017-08-03 12:23:02 -0700

    Update Boosting.py

--- Boosting.py
+++ Boosting.py
@@ -15,3 +15,28 @@ rcParams['figure.figsize'] = 12, 4
 train = pd.read_csv('train_modified.csv')
 target = 'Disbursed'
 IDcol = 'ID'
+
+
+def modelfit(alg, dtrain, predictors, perforCV = True, printFeatureImportance = True, cv_folds = 5):
+    alg.fit(dtrain[predictors], dtrain['Disbursed'])
+
+    dtrain_predictions = alg.predict(dtrain[predictors])
+    dtrain_predprob = alg.predict_proba(dtrain[predictors])[:,1]
+
+    if performCV:
+        cv_score = cross_validation.cross_val_score(alg, dtrain[predictors], dtrain['Disbursed'], cv=cv_folds, scoring='roc_auc')
+    
+    #Print model report:
+    print("\nModel Report")
+    print("Accuracy : %.4g" % metrics.accuracy_score(dtrain['Disbursed'].values, dtrain_predictions))
+    print("AUC Score (Train): %f" % metrics.roc_auc_score(dtrain['Disbursed'], dtrain_predprob))
+    
+    if performCV:
+        print("CV Score : Mean - %.7g | Std - %.7g | Min - %.7g | Max - %.7g" % (np.mean(cv_score),np.std(cv_score),np.min(cv_score),np.max(cv_score)))
+        
+    #Print Feature Importance:
+    if printFeatureImportance:
+        feat_imp = pd.Series(alg.feature_importances_, predictors).sort_values(ascending=False)
+        feat_imp.plot(kind='bar', title='Feature Importances')
+        plt.ylabel('Feature Importance Score')
+
