9bd1c43fd45472aa21fe7c7b3b655fed7a433485
jjeble <jayj2019@gmail.com>
2017-08-04 13:29:44 -0700

    Update Newtry.py

--- Newtry.py
+++ Newtry.py
@@ -1,8 +1,36 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import io
 import requests
 url="https://raw.githubusercontent.com/fivethirtyeight/data/master/college-majors/recent-grads.csv"
 s=requests.get(url).content
 df=pd.read_csv(io.StringIO(s.decode('utf-8')))
 
-print(df.head)
+temp1 = df['Major_category'].value_counts(ascending = True)
+temp2 = df.pivot_table(values = 'Unemployment_rate', index = ['Major_category'],aggfunc = lambda x: x.mean())
+#temp2 = df.pivot_table(values = '
+print(temp1)
+print(temp2)
+
+fig = plt.figure(figsize=(8,4))
+ax1 =  fig.add_subplot(121)
+ax1.set_xlabel('Major Category')
+ax1.set_ylabel('Count of people')
+#temp1.plot(kind = 'bar')
+
+ax2 =  fig.add_subplot(122)
+#temp2.plot(kind='bar')
+ax2.set_xlabel('Category')
+ax2.set_ylabel('Probablility of Unemployment')
+
+temp3 = pd.crosstab(df['Major_category'],df['Unemployment_rate'])
+print(temp3)
+
+
+
+
+
+
+plt.show()
+
