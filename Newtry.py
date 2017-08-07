92ecdbca9a5b38b8722bbf0efe26742e6f6eaad4
jjeble <jayj2019@gmail.com>
2017-08-07 11:37:36 -0700

    Update Newtry.py

--- Newtry.py
+++ Newtry.py
@@ -9,23 +9,15 @@ df=pd.read_csv(io.StringIO(s.decode('utf-8')))
 
 temp1 = df['Major_category'].value_counts(ascending = True)
 temp2 = df.pivot_table(values = 'Unemployment_rate', index = ['Major_category'],aggfunc = lambda x: x.mean())
-#temp2 = df.pivot_table(values = '
-print(temp1)
-print(temp2)
 
-fig = plt.figure(figsize=(8,4))
-ax1 =  fig.add_subplot(121)
-ax1.set_xlabel('Major Category')
-ax1.set_ylabel('Count of people')
-#temp1.plot(kind = 'bar')
+print(df.apply(lambda x:sum(x.isnull()),axis=0))
 
-ax2 =  fig.add_subplot(122)
-#temp2.plot(kind='bar')
-ax2.set_xlabel('Category')
-ax2.set_ylabel('Probablility of Unemployment')
+df['Men'].fillna(int(df['Men'].mean()), inplace = True)
+df['Women'].fillna(int(df['Women'].mean()), inplace = True)
+df['Total'].fillna(df['Men']+df['Women'], inplace = True)
+print(df.head(25))
 
-temp3 = pd.crosstab(df['Major_category'],df['Unemployment_rate'],aggfunc = lambda x: x.mean())
-print(temp3)
+
 
 
 
