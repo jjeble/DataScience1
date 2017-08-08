065f6edc64bf70d0cd604756de1e46b32e2e4e29
jjeble <jayj2019@gmail.com>
2017-08-08 13:47:56 -0700

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import quandl
 import math
-
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 df = quandl.get('WIKI/GOOGL')
 
 
@@ -22,3 +24,20 @@ forecast_out = int(math.ceil(0.01*len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
 print(df.head())
+
+X = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+
+X  = preprocessing.scale(X)
+
+
+df.dropna(inplace = True)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size = 0.2)
+clf = LinearRegression()
+
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test,y_test)
+print(accuracy)
