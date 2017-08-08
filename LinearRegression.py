c657cd776b482e11c512538a05706dee9b6e3710
jjeble <jayj2019@gmail.com>
2017-08-08 13:09:44 -0700

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,5 +1,6 @@
-import pandas as pd
+\import pandas as pd
 import quandl
+import math
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -13,4 +14,11 @@ df['PCT_change'] = (df['Adj. Close'] -df['Adj. Open'])/ df['Adj. Open'] * 100.0
 
 df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
 
+forecast_col = 'Adj. Close'
+
+df.fillna(-99999, inplace = True)
+
+forecast_out = int(math.ceil(0.01*len(df)))
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
 print(df.head())
