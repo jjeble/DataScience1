42d4e2ccb88e6c8fae4e3e59166a0cef61ccfe4f
jjeble <jayj2019@gmail.com>
2017-08-09 13:18:56 -0700

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -6,6 +6,7 @@ from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
+import pickle
 style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
@@ -49,6 +50,11 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_si
 clf = LinearRegression()
 
 clf.fit(X_train, y_train)
+with open('linearregression.pickle','wb') as f:
+    pickle.dump(clf,f)
+pickle_in = open('linearregression.pickle','rb')
+clf = pickle.load(pickle_in)
+            
 
 accuracy = clf.score(X_test,y_test)
 
@@ -67,7 +73,7 @@ for i in forecast_set:
     df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
 df['Adj. Close'].plot()
 df['Forecast'].plot()
-
+print(df.tail(50))
 plt.legend(loc=4)
 plt.xlabel('Date')
 plt.ylabel('Price')
