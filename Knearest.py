16687ac9c838d7637609294e9eed02912b2b1c90
jjeble <jayj2019@gmail.com>
2017-08-22 15:01:07 -0700

    Update Knearest.py

--- Knearest.py
+++ Knearest.py
@@ -16,7 +16,7 @@ accuracy = clf.score(X_test,y_test)
 print(accuracy)
 
 example_measures= np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,2,2,2,3,2,1]])
-example_measures = example_measures.reshape(2,-1)
+example_measures = example_measures.reshape(len(example_measures),-1)
 prediction = clf.predict(example_measures)
 print(prediction)
 
