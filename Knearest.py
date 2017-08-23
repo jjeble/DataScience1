c66fbca11119ce8470476b27496a8a9669d27752
jjeble <jayj2019@gmail.com>
2017-08-23 16:56:41 -0700

    Update Knearest.py

--- Knearest.py
+++ Knearest.py
@@ -1,22 +1,8 @@
-import numpy as np
-from sklearn import preprocessing, cross_validation, neighbors
-import pandas as pd
-df = pd.read_csv('breast-cancer-wisconsin.data.txt')
-df.replace('?', -99999, inplace = True)
-df.drop(['id'],1,inplace=True)
+from math import sqrt
 
-X = np.array(df.drop(['class'],1))
-y = np.array(df['class'])
+plot1 = [1,3]
+plot2 = [2,5]
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y, test_size=0.2)
+euclidean_distance = sqrt((plot1[0] - plot2[0])**2+ (plot1[1] - plot2[1])**2)
 
-clf = neighbors.KNeighborsClassifier()
-clf.fit(X_train, y_train)
-accuracy = clf.score(X_test,y_test)
-print(accuracy)
-
-example_measures= np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,2,2,2,3,2,1]])
-example_measures = example_measures.reshape(len(example_measures),-1)
-prediction = clf.predict(example_measures)
-print(prediction)
-
+print(euclidean_distance)
