fb93ee351683d6499c4216395cc911efaf7d89e6
jjeble <jayj2019@gmail.com>
2017-08-23 18:15:35 -0700

    Update Knearest.py

--- Knearest.py
+++ Knearest.py
@@ -12,22 +12,32 @@ new_features = [5,7]
 def k_nearest_neighbors(data, predict, k=3):
     if len(data) >= k:
         warnings.warn('K is set to value less than total voting groups')
-    knnalgos
+    distances = []
+    for group in data:
+        for features in data[group]:
+            euclidean_distance = np.linalg.norm(np.array(features)-np.array(predict))
+            distances.append([euclidean_distance,group])
+    votes = [i[1] for i in sorted(distances)[:k]]
+    vote_result = Counter(votes).most_common(1)[0][0]
+    
+            
     return vote_result
+result = k_nearest_neighbors(dataset, new_features, 3)
+print(result)
 
 
 
 
 
 ##
-##for i in dataset:
-##    for ii in dataset[i]:
-##        plt.scatter(ii[0],ii[1],s=100,color = i)
-##
-##plt.scatter(new_features[0],new_features[1],s=100,color = 'brown')
-##
-##
-##plt.show()
+for i in dataset:
+    for ii in dataset[i]:
+        plt.scatter(ii[0],ii[1],s=100,color = i)
+
+plt.scatter(new_features[0],new_features[1],s=100,color = result)
+
+
+plt.show()
         
 
 
