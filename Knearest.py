dabe876294f0c3a273ec53b9ceecf36ca0ffff28
jjeble <jayj2019@gmail.com>
2017-08-30 14:06:35 -0700

    Update Knearest.py

--- Knearest.py
+++ Knearest.py
@@ -5,9 +5,9 @@ from matplotlib import style
 from collections import Counter
 style.use('fivethirtyeight')
 import warnings
+import pandas as pd
+import random
 
-dataset = {'k': [[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]]}
-new_features = [5,7]
 
 def k_nearest_neighbors(data, predict, k=3):
     if len(data) >= k:
@@ -22,24 +22,32 @@ def k_nearest_neighbors(data, predict, k=3):
     
             
     return vote_result
-result = k_nearest_neighbors(dataset, new_features, 3)
-print(result)
+
+df = pd.read_csv("breast-cancer-wisconsin.data.txt")
+df.replace('?',-99999,inplace=True)
+df.drop(['id'],1,inplace=True)
+full_data = df.astype(float).values.tolist()
+random.shuffle(full_data)
+
+test_size = 0.2
+train_set = {2:[],4:[]}
+test_set = {2:[],4:[]}
+train_data = full_data[:-int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):]
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
 
 
+correct = 0
+total = 0
 
-
-
-##
-for i in dataset:
-    for ii in dataset[i]:
-        plt.scatter(ii[0],ii[1],s=100,color = i)
-
-plt.scatter(new_features[0],new_features[1],s=100,color = result)
-
-
-plt.show()
-        
-
-
-#euclidean_distance = sqrt((plot1[0] - plot2[0])**2+ (plot1[1] - plot2[1])**2)
-
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set,data, k = 5)
+        if group == vote:
+            correct+=1
+        total+=1
+print('Accureacy: ',correct/total)
+            
