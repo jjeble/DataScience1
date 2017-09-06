4fcfce5d2c096c637cfd2e4c66d80d91259d56ee
jjeble <jayj2019@gmail.com>
2017-09-05 18:24:37 -0700

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -33,7 +33,32 @@ class Support_Vector_Machine:
             w =  np.array([latest_optimum,latest_optimum])
             optimized = False
             while not optimized:
-                pass
+                for b in np.arange(-1*(self.max_feature_value*b_range_multiple),
+                                   self.max_feature_value*b_range_multiple,step*b_multiple):
+                    for transformation in transforms:
+                        w_t  = w*transformation
+                        found_option = True
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                yi = i
+                                if not yi*(np.dot(w_t,xi)+b) >= 1:
+                                    found_option = False
+                        if found_option:
+                            opt_dict[np.linalg.norm(w_t)] = [w_t,b]
+                if w[0] <0:
+                    optimized = True
+                    print('optimized a step')
+                else:
+                    w = w-step
+            norms = sorted([n for n in opt_dict])
+            opt_choice  =opt_dict[norms[0]]
+            self.b = opt_choice[1]
+            self.w = opt_choice[0]
+            latest_optimium = opt_choice[0][0]
+            
+                                
+                        
+                
         
         
 
