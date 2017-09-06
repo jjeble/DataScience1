703aed62a99b9d64db7c09c0e9bf514be71820c4
jjeble <jayj2019@gmail.com>
2017-09-05 17:57:47 -0700

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -12,7 +12,30 @@ class Support_Vector_Machine:
             self.ax = self.fig.add_subplot(1,1,1)
 
     def fit(self,data):
-        pass
+        self.data = data
+        opt_dict  = {}
+        transforms = [[1,1],[-1,1],[-1,-1],[1,-1]]
+        all_data = []
+        for yi in self.data:
+            for featureset in self.data[yi]:
+                for feature in featureset:
+                    all_data.append(feature)
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+        all_data = Npne
+        step_sizes = [self.max_feature_value *0.1,
+                      self.max_feature_value *0.01,
+                      self.max_feature_value *0.001]
+        b_range_multiple = 5
+        b_multiple = 5
+        latest_optimum = self.max_feature_value*10
+        for step in step_sizes:
+            w =  np.array([latest_optimum,latest_optimum])
+            optimized = False
+            while not optimized:
+                pass
+        
+        
 
     def predict(self,features):
         classification = np.sign(np.dot(np.array(features),self.w)+self.b)
