40d227a27b612cc957442620588683212bd2abe6
jjeble <jayj2019@gmail.com>
2017-09-07 21:47:40 -0700

    Add files via upload

---
+++ udemy1.py
@@ -0,0 +1,68 @@
+
+
+
+import numpy as np
+
+np.zeroes(10)
+
+
+
+
+
+
+
+
+
+
+##
+##my_list = [1,2,3]
+##import numpy as np
+##arr = np.array(my_list)
+##
+##print(arr)
+##my_mat = [[1,2,3],[4,5,6],[7,8,9]]
+##
+##print(np.array(my_mat))
+##
+##
+##print(np.arange(0,11,2))
+##print(np.ones((2,3)))
+##
+##
+##print(np.linspace(0,5,100))
+##
+##
+##print(np.eye(4))
+##
+##print(np.random.rand(5))
+##print(np.random.rand(5,5,))
+##
+##
+##print()
+##
+##print(np.random.randn(3,3))
+##print()
+##
+##print(np.random.randint(1,100,10))
+##
+##print()
+##
+##arr = np.arange(25)
+##ranarr = np.random.randint(0,50,10)
+##
+##print(arr.reshape(5,5))
+##print(ranarr)
+##print(ranarr.max())
+##
+##print(ranarr.min())
+##
+##print(ranarr.argmax())
+##print(ranarr.argmin())
+##arr = arr.reshape(5,5)
+##
+##print(arr.shape)
+##
+##print(arr.dtype)
+##
+
+
